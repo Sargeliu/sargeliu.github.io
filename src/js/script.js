9509e9395d91dc860d13df848d8b23eb76829139
Sargeliu <sargeliu@mail.ru>
2020-10-02 18:57:55 +0500

    add  Slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,21 @@
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        // dots: true, //кружки снизу слайда
+        infinite: true, // повтор слайдов, после посмотров всех
+        autoplay: true, // автопереключение слайдов
+        speed: 1200, // скорость переключения слайдов
+        slidesToShow: 1, // количество слайдов одновременно
+        // adaptiveHeight: true, //автоматический подбор высоты
+        /* fade: true,
+        cssEase: 'linear' */ // эффектное переключение слайдов
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
 });
