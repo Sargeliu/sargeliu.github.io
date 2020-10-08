75c68f0220d60d999f7ed85437a9766ad489ffeb
Sargeliu <sargeliu@mail.ru>
2020-10-08 19:40:11 +0500

    add catalog(the second step)

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,38 @@
-// $(document).ready(function() {
-//     $('.carousel__inner').slick({
-//         // dots: true, //кружки снизу слайда
-//         infinite: true, // повтор слайдов, после посмотров всех
-//         autoplay: true, // автопереключение слайдов
-//         speed: 1200, // скорость переключения слайдов
-//         slidesToShow: 1, // количество слайдов одновременно
-//         // adaptiveHeight: true, //автоматический подбор высоты
-//         /* fade: true,
-//         cssEase: 'linear' */ // эффектное переключение слайдов
-//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-//         responsive: [{
-//             breakpoint: 768,
-//             settings: {
-//                 dots: true,
-//                 arrows: false
-//             }
-//         }]
-//     });
-// });
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        // dots: true, //кружки снизу слайда
+        infinite: true, // повтор слайдов, после посмотров всех
+        autoplay: true, // автопереключение слайдов
+        speed: 1200, // скорость переключения слайдов
+        slidesToShow: 1, // количество слайдов одновременно
+        // adaptiveHeight: true, //автоматический подбор высоты
+        /* fade: true,
+        cssEase: 'linear' */ // эффектное переключение слайдов
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-// add Tiny Slider
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
-});
-
-document.querySelector('.prev').addEventListener('click', function() {
-    slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function() {
-    slider.goTo('next');
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
